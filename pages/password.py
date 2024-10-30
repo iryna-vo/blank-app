fc3f34fddc9c82ce95a36f449871fa05eaf741b5
iryna-vo <iryna.vorobiova@outlook.com>
2024-10-30 17:56:02 +0100

    j

--- pages/password.py
+++ pages/password.py
@@ -1,8 +1,13 @@
 import streamlit as st
 
 st.title("Vérification de mot de passe")
-password = "mypassWord"
-saisie = st.text_input("Enter your password :")
-while saisie != password:
-    print("Error. Try again")
-print("Well done!")
+#password = "mypassWord"
+#saisie = st.text_input("Enter your password :")
+#while saisie != password:
+#    print("Error. Try again")
+#print("Well done!")
+password = st.text_input("Enter your password :", type = "password")
+if password:
+    if password == "mypassWord":
+        st.success("C'est bon!")
+    else: st.error("Mot de passe incorrect. Essayez à nouveau. !")
